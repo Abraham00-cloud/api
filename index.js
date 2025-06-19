3b8249a3934003a5e6ede84a9dd1d972ee7b78d8
Abraham Alagbe <alagbeabraham00@gmail.com>
2025-06-19 09:58:54 +0100

    Api class

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const home = '/'
+const port = 5550
+const htmlPage = '/html'
+
+const allStudents = [
+    {name: 'solomon', class:'100lvl', school:'sqi'},
+    {name: 'Emmanuel', class:'200lvl', school:'school'}
+
+]
+app.get(home, (req, res) => {
+    res.send('welcome to Node')
+})
+
+app.get('/dashboard', (req, res) => {
+    res.send('welcome to the fucking dashboard')
+})
+app.get(htmlPage, (req, res) => {
+    console.log('is it working')
+    res.sendFile(`${__dirname}/index.html`)
+})
+app.get('/api', (req, res) => {
+    try{
+        res.status(201).json({message: allStudents})
+    }
+    catch(error) {
+        res.status(503).json({message: error})
+    }
+})
+
+app.listen(port, () => {
+    console.log('this is so nice');  
+})
